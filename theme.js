0c32b6f1450c6648832fb9201258bb584ade615c
mepezho <09nms01@mail.ru>
2025-02-20 23:10:43 +0300

    Delete theme.js

--- theme.js
+++
@@ -1,7 +0,0 @@
-//выбираем кнопку
-const btn = document.querySelector(".btn-toggle");
-//отслеживаем щелчок по кнопке
-btn.addEventListener("click", function(){
-//переключение темы при нажатии на кнопку
-document.body.classList.toggle("dark.css");
-});
